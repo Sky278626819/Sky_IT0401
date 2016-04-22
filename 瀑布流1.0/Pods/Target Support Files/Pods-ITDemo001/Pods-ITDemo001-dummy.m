201e568329bed0c7b3f2052ef63fb766ee323cd2
Sky <mac@wangshichang.local>
2016-04-22 15:24:07 +0800

    Revert "Sky"
    This reverts commit b6a3682f5c0c16c79b73606409332708911f5304.

--- 瀑布流1.0/Pods/Target Support Files/Pods-ITDemo001/Pods-ITDemo001-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ITDemo001 : NSObject
-@end
-@implementation PodsDummy_Pods_ITDemo001
-@end
--- 瀑布流1.0/Pods/Target Support Files/Pods-ITDemo001/Pods-ITDemo001-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_ITDemo001VersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ITDemo001VersionString[];
-
